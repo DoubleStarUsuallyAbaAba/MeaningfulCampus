d7e6901041932eaf2c04172c7a93123e29085c09
DoubleStarIsSingle <105093888+DoubleStarIsSingle@users.noreply.github.com>
2023-08-05 02:33:17 +0800

    Add files via upload

---
+++ 【数据结构】/【递归】迷宫问题/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include "maze.h"
+using namespace std;
+
+int main()
+{
+	Maze<char> maze(CLEAR,'*');//实例化迷宫类，用空格初始化迷宫数组，用字符*作为路线的开头标识符
+
+	//设置四周围墙
+	for (int i = 0; i < X; i++)
+	{
+		maze.set_stone(0, i, STONE);
+		maze.set_stone(Y-1, i, STONE);
+	}
+	for (int i = 1; i < Y-1; i++)
+	{
+		maze.set_stone(i, 0, STONE);
+		maze.set_stone(i, X-1, STONE);
+	}
+	//设置路障、迷宫起点与终点
+	maze.set_stone(1, 2, STONE);
+	maze.set_stone(2, 2, STONE);
+	maze.set_stone(4, 2, STONE);
+	maze.set_stone(5, 2, STONE);
+	maze.set_stone(2, 4, STONE);
+	maze.set_stone(3, 4, STONE);
+	maze.set_stone(5, 4, STONE);
+	maze.set_stone(2, 5, STONE);
+	maze.set_stone(Y_START, X_START, START);
+	maze.set_stone(Y_END, X_END, END);
+	//寻路
+	maze.find_route(Y_START, X_START, FLAG, CLEAR, END, STONE);
+	//展示路线
+	maze.show_route();
+
+	//******************************************************************************************************
+    //******************************************************************************************************
+	cout << endl << "程序到此结束，请按回车键继续，此处主要是为了防止程序闪退" << endl;
+	int _ = getchar();
+	//******************************************************************************************************
+	//******************************************************************************************************
+	return 0;
+}
---
+++ 【数据结构】/【递归】迷宫问题/maze.h
@@ -0,0 +1,102 @@
+﻿#pragma once
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+#define Y 7
+#define X 7//宏定义迷宫尺寸，便于修改
+#define SPACE 2
+#define Y_START 0//起点坐标y
+#define X_START 1//起点坐标x
+#define Y_END 6 //终点坐标y
+#define X_END 5 //终点坐标x
+#define FLAG '*' //标记路线的标记
+#define CLEAR ' ' //表示迷宫中不是路障的地方的标识符
+#define START 'S' //路线起点标记
+#define END 'F' //路线终点标记
+#define STONE '#' //路障标识符
+
+template<class T>
+class Maze
+{
+private:
+	T map[Y][X];//迷宫数组地图
+	string route;//用于记录路线
+public:
+	Maze(T a,char b)//构造函数
+	{
+		for (int i = 0; i < Y; i++)
+			for (int j = 0; j < X; j++)
+				map[i][j] = a;
+		route = b;
+	}
+	void set_stone(int y, int x, T a)//在坐标(x,y)设置迷宫路障
+	{
+		map[y][x] = a;
+	}
+	int find_route(int y, int x,T flag,T clear,T end, T stone);//寻路函数
+	void show_route();//输出路径
+};
+template<class T>
+int Maze<T>::find_route(int y, int x,T flag, T clear, T end,T stone)//寻路函数
+{
+	if (map[y][x] == end)//递归终止条件，如果找到终点，则递归结束，并标记该坐标，成为路径的一部分，返回1
+	{
+		map[y][x] = flag;
+		return 1;
+	}
+	else if (map[y][x] == stone)//如果目标位置被路障挡住，则此路不通，返回0
+		return 0;
+	else
+	{
+		map[y][x] = flag;//目标位置未被路障挡住
+		route +=to_string(y);//路径中加入该步的y
+		route +=to_string(x);//路径中加入该步的x
+		if (y - 1 >= 0 && map[y - 1][x] != flag && find_route(y - 1, x, flag,clear, end,stone))//向上寻路
+			return 1;
+		else if (y + 1 >= 0 && map[y + 1][x] != flag && find_route(y + 1, x, flag, clear, end, stone))//向下寻路
+			return 1;
+		else if (x - 1 >= 0 && map[y][x - 1] != flag && find_route(y, x - 1, flag, clear, end, stone))//向左寻路
+			return 1;
+		else if (x + 1 >= 0 && map[y][x + 1] != flag && find_route(y, x + 1, flag, clear, end, stone))//向右寻路
+			return 1;
+		else
+		{//如果走入死胡同则回退，尝试其他路径，并且在路径中清除错误路线的标记
+			route = route.substr(0, route.length() - 2);
+			map[y][x] = clear;
+			return 0;
+		}
+	}
+}
+template<class T>
+void Maze<T>::show_route()
+{
+	//标记路线起止点并且在控制台显示迷宫与路线
+	set_stone(Y_START, X_START, START);
+	set_stone(Y_END, X_END, END);
+	cout << setiosflags(ios::left);
+	cout << "迷宫地图:" << endl;
+	cout << setw(SPACE) << ' ';
+	for (int i = 0; i < X; i++)
+		cout << setw(SPACE) << i;
+	cout << endl;
+	for (int i = 0; i < Y; i++)
+	{
+		cout << setw(SPACE) << i;
+		for (int j = 0; j < X; j++)
+			cout << setw(SPACE) << map[i][j];
+		cout << endl;
+	}
+
+	//输出迷宫路径
+	cout << endl << "迷宫路径:" << endl << endl;
+	for (int i = 1, j = route.length(); i < j; i+=2)
+	{
+		cout << " <" << route[i] << ',' << route[i + 1] << "> ";
+		if (i != j - 2)
+			cout << "-->";
+		if ((i+1)/2 % 5 == 0)
+			cout << endl;
+	}
+	cout << endl << resetiosflags(ios::left);
+}
